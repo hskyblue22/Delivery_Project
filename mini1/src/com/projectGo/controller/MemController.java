c17c683157c4f14ee7e5301ac8014178a5c1eeca
kmj381900 <kmj381912@naver.com>
2021-06-05 22:08:04 +0900

    21/06/05 개발완료

---
+++ mini1/src/com/projectGo/controller/MemController.java
@@ -0,0 +1,15 @@
+package com.projectGo.controller;
+
+public class MemController {
+	//개인정보관리
+	//수정-->이름,이메일,휴대폰번호,주소
+	//+탈퇴하기
+	private Member mem=new Member();
+	
+	public void update(Member m) {
+		boolean chk=true;
+		
+		for()
+	}
+	
+}
