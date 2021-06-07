35e37c1767e73ff9c8aa6723dea9af9ce366bff3
kmj381900 <kmj381912@naver.com>
2021-06-07 20:23:21 +0900

    개발 완료 21/06/07

---
+++ mini1/src/com/projectGo/run/MemberRun.java
@@ -0,0 +1,14 @@
+package com.projectGo.run;
+
+import com.projectGo.view.MemberSignInFrame;
+
+public class MemberRun {
+
+	public static void main(String[] args) {
+		
+		
+		MemberSignInFrame f = new MemberSignInFrame();
+	
+	}
+
+}
