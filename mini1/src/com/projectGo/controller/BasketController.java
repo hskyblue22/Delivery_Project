cc1d89755eba2e22b1b482d7b4765ae29394a367
HONG YEONG EUN <HONG@HONG-PC>
2021-06-09 16:02:05 +0900

    basket, order 관련 기능수정 2021-06-09 16:02

--- mini1/src/com/projectGo/controller/BasketController.java
+++ mini1/src/com/projectGo/controller/BasketController.java
@@ -1,97 +1,111 @@
 package com.projectGo.controller;
 
 
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map.Entry;
 import java.util.Set;
 
-import com.projectGo.model.dao.BasketDao;
+import com.projectGo.model.vo.Basket;
 import com.projectGo.model.vo.Menu;
+import com.projectGo.model.vo.Order;
 import com.projectGo.view.OrderView;
 
 public class BasketController {
-
-	private BasketDao basDao = new BasketDao();
+	
+	private Basket basket;
+	
 	
 	public BasketController() {}
 	
 	
-	//메뉴리스트 리턴
-	public HashMap<String, Menu> getMenuList() {
-		return basDao.getMenuList();              
-	}
-	
-	
-	// 장바구니메뉴조회
-	public Iterator<Entry<String, Menu>> selectedMenu() {
-
-		Set<Entry<String, Menu>> menuSet = basDao.getMenuList().entrySet();
-		Iterator<Entry<String, Menu>> iterMenu = menuSet.iterator();
-
-		return iterMenu;
-
-	}
-
-	// 삭제할 메뉴명 받아 삭제
-	public void deleteBasketMenu(String menuName) {
-
-		basDao.deleteBasketMenu(menuName);
-
-	}
-
-	// 전체 삭제
-	public void deleteAll() {
-
-		basDao.deleteAll();
-
-	}
-
-	// 메뉴명 받아 수량 변경
-	public void modifyQuantity(String Menu, int modifyQuantity) {
-
-		basDao.modifyQuantity(Menu, modifyQuantity);
-
-	}
-
-	// 주문버튼 누르면 오더페이지로 넘어가면서 객체 전달
-	public void order() {
-		
-		basDao.basketOrder();
-		new OrderView();
+	// 장바구니 담기 하고 장바구니를 누르면 basket객체 전달
+	public BasketController(Basket basket) {
+		this.basket = basket;
 		
 	}
-
+	
+	public Basket getBasket() {
+		return basket;
+	}
+		
+	
+	
 	// 리스트 크기 반환
 	public int getMenuListSize() {
 
-		return basDao.getMenuList().size();
+		return basket.getMenuList().size();
 
 	}
 
 	// 리스트가 비어있는지 확인
 	public boolean listIsEmpty() {
 
-		return basDao.getMenuList().isEmpty();
+		return basket.getMenuList().isEmpty();
+
+	}
+	
+	
+	// 장바구니메뉴조회
+	public Iterator<Entry<String, Menu>> selectedMenu() {
+
+		Set<Entry<String, Menu>> menuSet = basket.getMenuList().entrySet();
+		Iterator<Entry<String, Menu>> iterMenu = menuSet.iterator();
+
+		return iterMenu;
 
 	}
 
+	
+	// 삭제할 메뉴명 받아 삭제
+	public void deleteBasketMenu(String menuName) {
+
+		basket.getMenuList().remove(menuName);
+
+	}
+	
+
+	// 전체 삭제
+	public void deleteAll() {
+
+		basket.getMenuList().clear();
+
+	}
+
+	
+	// 메뉴명 받아 수량 변경
+	public void modifyQuantity(String Menu, int modifyQuantity) {
+
+		basket.getMenuList().get(Menu).setQuantity(modifyQuantity);
+		
+	}
+
+	
 	// 메뉴명에 해당하는 수량 리턴
 	public int getMenuQuantity(String menu) {
 
-		return basDao.getMenuList().get(menu).getQuantity();
+		return basket.getMenuList().get(menu).getQuantity();
 	}
 
+	
 	// 메뉴 가격 리턴
 	public int getMenuPrice(String menu) {
 
-		return basDao.getMenuList().get(menu).getMenuPrice();
+		return basket.getMenuList().get(menu).getMenuPrice();
 	}
 
+	
 	// 식당명 리턴
 	public String getStoreName() {
 
-		return basDao.getBasket().getStoreName();
+		return basket.getStoreName();
+	}
+	
+	
+	// 주문버튼 누르면 오더페이지로 넘어가면서 객체 전달
+	public void order() {
+		
+		new OrderView(new Order(basket));
+		
 	}
 
 }
--- mini1/src/com/projectGo/model/dao/OrderDao.java
+++ mini1/src/com/projectGo/model/dao/OrderDao.java
@@ -14,72 +14,12 @@ import com.projectGo.model.vo.Order;
 public class OrderDao {
 	
 	private Order order;
-//	private HashMap<String, Menu> menuList;
 	
-//	private BasketDao basDao = new BasketDao();
-	
-	/*
-	private int payment;
-	private String request;
-	private Calendar orderedDate;
-	private String userAddress; */
-	
-	public OrderDao() {
-	}
+	public OrderDao() {}
 
-//	public OrderDao() {
-//		this.order = basDao.basketOrder();
-//	}
-	
-	public OrderDao(Order order) {
-		this.order = order;
-	}
-
-	
-	public Order getOrder() {
-		return order;
-	}
-	
-	
-	public HashMap<String, Menu> getMenuList() {
-		return order.getMenuList();              //NullPointerException
-	}
-
-	
-	//요청사항 저장
-	public void setRequest(String request) {
-		
-		order.setRequest(request);
-		
-	}
-
-	//배달팁 반환
-	public int getDeliveryTip() {
-		
-		return order.getDeliveryTip();
-	}
-
-	//결제금액 저장
-	public void setPayment(int totalPayment) {
-
-		order.setPayment(totalPayment);
-		
-	}
-	
-	//날짜 저장
-	public void setDate() {
-		
-		order.setOrderedDate(Calendar.getInstance());
-		
-	}
-	
-	//주소 저장
-	public void setUserAddress(String userAddress) {
-		order.setUserAddress(userAddress);
-	}
 
 	//order객체 저장 output
-	public void orderOutput() {
+	public void orderOutput(Order order) {
 		
 		try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("order_list.txt", true))) {
 			
